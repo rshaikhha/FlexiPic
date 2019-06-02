9c2c2a93b1e3a401a0d70c844f5d9b6ed3a832d6
rshaikhha <rshaikhha@users.noreply.github.com>
2019-06-02 18:59:47 +0430

    Updates

---
+++ precache-manifest.85fe6cb6a2b8ffee630331c2f056dace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911a6266c2eab3dbea61356ade8e6f69",
+    "url": "/flexipic/index.html"
+  },
+  {
+    "revision": "debda85fbce4ab938d31",
+    "url": "/flexipic/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/flexipic/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "debda85fbce4ab938d31",
+    "url": "/flexipic/static/js/main.2dbdcdaf.chunk.js"
+  },
+  {
+    "revision": "45d58c7181b034de7c81",
+    "url": "/flexipic/static/js/runtime~main.7d25ff73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flexipic/static/media/logo.5d5d9eef.svg"
+  }
+]);
